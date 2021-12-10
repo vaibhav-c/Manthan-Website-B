7ace8cbc1125f6318ed7db8a67c822d14f59ae99
vaibhav-c <vaibhav.c987@gmail.com>
2021-12-10 12:40:17 +0530

    some changes

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template
+from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 port = 5000
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
 def home():
